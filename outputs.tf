80ece994f96820ed3fa2e68ecc057303db184002
JuanJDlp <juanjosedelapava1@gmail.com>
2026-04-02 11:54:56 -0500

    feat: added outputs

---
+++ outputs.tf
@@ -0,0 +1,54 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint URL for the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded certificate data for the EKS cluster"
+  value       = module.eks.cluster_ca_certificate
+}
+
+output "oidc_provider_url" {
+  description = "URL of the OIDC provider for the EKS cluster"
+  value       = module.eks.oidc_provider_url
+}
+
+output "oidc_provider_arn" {
+  description = "ARN of the OIDC provider for the EKS cluster"
+  value       = module.eks.oidc_provider_arn
+}
+
+output "node_group_id" {
+  description = "ID of the created EKS node group"
+  value       = module.node_groups.node_group_id
+}
+
+output "node_group_arn" {
+  description = "ARN of the created EKS node group"
+  value       = module.node_groups.node_group_arn
+}
+
+output "ecr_repository_urls" {
+  description = "Map of microservice names to ECR repository URLs"
+  value       = module.ecr.ecr_repository_urls
+}
